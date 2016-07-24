e0eb3c318dd27278294f174f9ad32dc3eeddde1e
Joan Saez <janisaez@gmail.com>
2016-07-24 01:52:43 +0100

    new count

--- app/jobs/interval/calculate_votes_by_intervals.rb
+++ app/jobs/interval/calculate_votes_by_intervals.rb
@@ -24,20 +24,19 @@ module Interval
       time = time_step
 
       interval_values = @video.interval_values
-      votes = Vote.where("video_id = ?", @video.youtube_id)
+      @votes = Vote.where("video_id = ?", @video.youtube_id)
 
       if (interval_values.count > 0)
         interval_values.each do |v|
-          votes_positive_count = votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='up'", v.start, v.end).count()
-          votes_negative_count = votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='down'", v.start, v.end).count()
-          v.update_attributes!(votes: votes_positive_count-votes_negative_count)
+          total_votes = calculate_votes(v.start, v.end)
+          v.update_attributes!(votes: total_votes)
         end
       else
-        # total_votes = []
         while initial_time < total_time do
-          votes_positive_count = votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='up'", initial_time, time).count()
-          votes_negative_count = votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='down'", initial_time, time).count()
-          video.interval_values.create!(action: 'up/down', start: initial_time, end: time, votes: votes_positive_count-votes_negative_count)
+          total_votes = calculate_votes(initial_time, time)
+
+          video.interval_values.create!(action: 'up/down', start: initial_time, end: time, votes: total_votes)
+
           # total_votes << {up: votes_positive_count, down: votes_negative_count, time: time}
           initial_time = initial_time + time_step
           time = time + time_step
@@ -46,6 +45,51 @@ module Interval
 
 
       # render json: {data: total_votes }
+    end
+
+    def calculate_votes(initial_time, time)
+      all_up_votes = @votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='up'", initial_time, time)
+      up_votes_sorted = all_up_votes.order('created_at DESC')
+      up_user_ids = up_votes_sorted.map(&:user_id).uniq
+
+      up_votes=[]
+      up_user_ids.each do |u_id|
+        up_votes << up_votes_sorted.map{|a| (a[:user_id]==u_id) ? a:nil}.compact.first
+      end
+
+      all_down_votes = @votes.where("vote_stamp >= ? AND vote_stamp < ? AND action='down'", initial_time, time)
+      down_votes_sorted = all_down_votes.order('created_at DESC')
+      down_user_ids = down_votes_sorted.map(&:user_id).uniq
+
+      down_votes=[]
+      down_user_ids.each do |u_id|
+        down_votes << down_votes_sorted.map{|a| (a[:user_id]==u_id) ? a:nil}.compact.first
+      end
+
+      final_up_votes =[]
+      final_down_votes =[]
+      up_votes.each do |u_vote|
+        d_vote = down_votes.map{|a| (a[:user_id]==u_vote.user_id) ? a:nil}.compact.first
+        unless d_vote.nil?
+          if d_vote.created_at < u_vote.created_at
+            final_up_votes << u_vote
+          else
+            final_down_votes << d_vote
+          end
+        else
+          final_up_votes << u_vote
+        end
+      end
+
+      down_votes.each do |d_vote|
+        u_vote = final_up_votes.map{|a| (a[:user_id]==d_vote.user_id) ? a:nil}.compact.first
+        if u_vote.nil?
+          already_there = final_down_votes.map{|a| (a[:user_id]==d_vote.user_id) ? a:nil}.compact.first
+          final_down_votes << d_vote if already_there.nil?
+        end
+      end
+
+      final_up_votes.count - final_down_votes.count
     end
 
   end
